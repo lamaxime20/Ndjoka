e815d5317b50c5643b94bce5128b9e91e7c1adc6
lamaxime20 <lamaxime20@gmail.com>
2026-05-27 17:50:34 +0200

    build ok

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -6,8 +6,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
         },
       },
     },
