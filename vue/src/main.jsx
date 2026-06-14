8f19a9e6935685331c0892a7397d36776029d117
lamaxime20 <lamaxime20@gmail.com>
2026-06-14 10:49:33 +0200

    Amélioration pour le déployement sur Vercel

--- vue/src/main.jsx
+++ vue/src/main.jsx
@@ -1,12 +1,3 @@
-import '@fontsource/poppins/500.css';
-import '@fontsource/poppins/600.css';
-import '@fontsource/poppins/700.css';
-import '@fontsource/poppins/800.css';
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/600.css';
-import '@fontsource/inter/700.css';
-import 'material-symbols/outlined.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
