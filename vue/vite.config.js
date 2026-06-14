97c5e26f93a06e49bd04c18c945ff6fb51263d0e
lamaxime20 <lamaxime20@gmail.com>
2026-06-14 11:04:49 +0200

    corrections

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['postcss'],
+  },
   build: {
     rollupOptions: {
       output: {
@@ -21,6 +25,9 @@ export default defineConfig({
           }
         },
       },
+    },
+    commonjsOptions: {
+      include: [/node_modules/],
     },
   },
 })
